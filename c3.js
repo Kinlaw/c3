0464afe789aefaaf486740207204c78b95d88d85
Josh Kinlaw <jkinlaw@redhat.com>
2016-02-16 08:54:36 -0500

    working on adding maxAngle

--- c3.js
+++ c3.js
@@ -41,6 +41,7 @@
         // bind "this" to nested API
         (function bindThis(fn, target, argThis) {
             Object.keys(fn).forEach(function (key) {
+                console.log(key);
                 target[key] = fn[key].bind(argThis);
                 if (Object.keys(fn[key]).length > 0) {
                     bindThis(fn[key], target[key], argThis);
